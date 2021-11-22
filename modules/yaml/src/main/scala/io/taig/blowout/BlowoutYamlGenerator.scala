e880400902e4f93c02f2a8c1346b295bcc473520
Niklas Klein <mail@taig.io>
2021-11-22 12:12:31 +0100

    Disable yaml printer line splitting

--- modules/yaml/src/main/scala/io/taig/blowout/BlowoutYamlGenerator.scala
+++ modules/yaml/src/main/scala/io/taig/blowout/BlowoutYamlGenerator.scala
@@ -5,7 +5,7 @@ import io.circe.yaml.Printer
 import sbt.File
 
 object BlowoutYamlGenerator {
-  val DefaultPrinter: Printer = Printer.spaces2.copy(preserveOrder = true)
+  val DefaultPrinter: Printer = Printer.spaces2.copy(preserveOrder = true, splitLines = false)
 
   def apply(target: File, content: () => Json, printer: Printer = DefaultPrinter): BlowoutGenerator =
     BlowoutGenerator(target, () => printer.pretty(content()))
